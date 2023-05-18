f50402c4392a9845af6031079381b87456cf1d91
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-05-18 16:49:44 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script>
+//Definiamo il nome del componente ed eventualmente i componenti che usa (nessuno)
+export default {
+    name: "AppHero"
+}
+</script>
+
+<!-- Quando si importa questo componente verrà visualizzato questo HTML al suo posto -->
+<template>
+    <div>
+        <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+                featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+        </div>
+    </div>
+</template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+//Definiamo il nome del componente ed eventualmente i componenti che usa (nessuno)
+export default {
+    name: "AppTitle"
+}
+</script>
+
+<!-- Quando si importa questo componente verrà visualizzato questo HTML al suo posto -->
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+        <img alt="Vue logo" class="logo" src="../assets/vue.svg" width="125" height="125" />
+    </div>
+</template>
