0405ff1a9cbcbcdcfb2ffc77eb05466cb5a1632d
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-05-18 17:24:40 +0200

    add : boodstrapp in js

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -8,7 +8,7 @@ export default {
 <!-- Quando si importa questo componente verrà visualizzato questo HTML al suo posto -->
 <template>
     <div>
-        <div class="jumbotron">
+        <div class="jumbotron bg-primary">
             <h1 class="display-4">Hello, world!</h1>
             <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
                 featured content or information.</p>
